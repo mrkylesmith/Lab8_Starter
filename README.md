8c8d85aac7947ff69edd5d1ce1ccf917c2e0d8d3
mrkylesmith <soccerkws@gmail.com>
2023-05-28 23:51:55 -0700

    Finished with Lab 8

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Lab 8 - Starter
+Author: Kyle Smith
+
+# 'Check Your Understanding' Questions:
+Question 1) Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.
+- Answer: Within a Github Action that runs whenever code is pushed to the remote repository. I would put the automated test suites in a GitHub Action that runs whenever the code is pushed because it ensures that the developer's code will be automatically tested before it is merged into the team repository, without relying on the developer to remember to manually test their code before pushing it. It also ensures that a consistent test suite is used by every member of the team, and whenever new tests are written, they can immediately be integrated into the GitHub Action test suite.  This will help prevent bugs from making their way into the repo, but it is reliant on having test coverage of the entire repo and integrating new tests into the GitHub Action routinely.
+
+Question 2) Would you use an end to end test to check if a function is returning the correct output? (yes/no)
+- Answer: No, you could use a unit test for that. End to end testing is used to automate tests that involve a user's actions through the UI from start to finish as a whole, and is not intended on testing small individual functions for correctness. A unit test would be appropriate for testing if a function is returning the correct output.
+
+Question 3) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+- Answer: No, I would use end-to-end testing for this "message" feature.  Since the "message" feature represents the core user action taken in this application (messaging application), it would be more appropriate to test the user's action from start to finish writing and sending the message in this application. This could involve many functions and is better suited for end-to-end testing, and since this functionality cannot be encapulated in a single function you should not use a unit test in this case.
+
+
+Question 4) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+- Answer: Yes, I would use a unit test for the "max message length" feature because this test could be isolated to a single component (function) that, for example takes a message as input, and simply checks if its length exceeds the max allowed size.  Since this test could be encapsulated to just a single function and test it's functionality, it would be ideal for a unit test.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,5 +1,111 @@
 // unit.test.js
-
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+// Unit tests for isPhoneNumber
+test('[validPhoneNumber] checks when valid phone number is given', () => {
+    const validPhoneNumber = "760-858-2903";
+    expect(functions.isPhoneNumber(validPhoneNumber)).toBe(true);
+  });
+test('[validPhoneNumber] checks when valid phone number is given', () => {
+    const validPhoneNumber = "666-499-3329";
+    expect(functions.isPhoneNumber(validPhoneNumber)).toBe(true);
+  });
+test('[validPhoneNumber] checks phone number given with short area code', () => {
+    // Phone number given with too short area code
+    const shortPhoneNumber = "66-499-329";
+    expect(functions.isPhoneNumber(shortPhoneNumber)).toBe(false);
+  });
+test('[validPhoneNumber] checks if invalid phone number given', () => {
+    const invalidPhoneNumber = "666483329";
+    expect(functions.isPhoneNumber(invalidPhoneNumber)).toBe(false);
+  });
+
+  // Unit tests for isEmail
+test('[isEmail] checks when valid email is given', () => {
+    const validEmail = "kyle@ucsd.edu";
+    expect(functions.isEmail(validEmail)).toBe(true);
+  });
+test('[isEmail] checks when valid email is given', () => {
+    const validEmail = "hikerboi@strava.com";
+    expect(functions.isEmail(validEmail)).toBe(true);
+  });
+test('[isEmail] checks when invalid email given without @ symbol', () => {
+    // Test invalid email that doesn't contain @ symbol
+    const invalidEmail = "hikerboistrava.com";
+    expect(functions.isEmail(invalidEmail)).toBe(false);
+  });
+test('[isEmail] checks when invalid email given without valid extension', () => {
+    // Test invalid email without .com (or any . extension)
+    const invalidEmail = "kyle@ucsd";
+    expect(functions.isEmail(invalidEmail)).toBe(false);
+  });
+
+// Unit tests for isStrongPassword
+test('[isStrongPassword] checks if too short password is given', () => {
+    // Test too short password 
+    const shortPassword = "Sam";
+    expect(functions.isStrongPassword(shortPassword)).toBe(false);
+  });
+test('[isStrongPassword] checks if password that is too long is given', () => {
+    // Test too long password
+    const longPassword = "iLoveMyDogSammy123"
+    expect(functions.isStrongPassword(longPassword)).toBe(false);
+  });
+test('[isStrongPassword] checks if password with invalid characters is given', () => {
+    // Test password with invalid characters
+    const invalidCharPassword = "$Sammy123"
+    expect(functions.isStrongPassword(invalidCharPassword)).toBe(false);
+  });
+test('[isStrongPassword] checks if correct password is given', () => {
+    // Test valid password
+    const validPassword = "Sammy123";
+    expect(functions.isStrongPassword(validPassword)).toBe(true);
+  });
+test('[isStrongPassword] checks if correct password is given', () => {
+    // Test valid password, testing underscore
+    const validPassword = "Sammy_123";
+    expect(functions.isStrongPassword(validPassword)).toBe(true);
+  });
+
+// Unit tests for isDate
+test('[isDate] tests invalid date with 2 digit year abbreviation', () => {
+    // Test invalid date with 2 digits given for year
+    const invalidDate = "05/28/23";
+    expect(functions.isDate(invalidDate)).toBe(false);
+  });
+test('[isDate] tests invalid date format, without day given', () => {
+    // Test invalid date with 2 digits given for year
+    const invalidDate = "/28/23";
+    expect(functions.isDate(invalidDate)).toBe(false);
+  });
+test('[isDate] tests valid date format given', () => {
+    // Test valid date
+    const validDate = "05/28/2023";
+    expect(functions.isDate(validDate)).toBe(true);
+  });
+test('[isDate] tests valid date format given', () => {
+    // Test valid date, 1 digit given for day
+    const validDate = "5/28/2023";
+    expect(functions.isDate(validDate)).toBe(true);
+  });
+// Unit tests for isHexColor
+test('[isHexColor] tests invalid hex color, too short', () => {
+    // Test invalid hex, only 5 digits given
+    const invalidHex = "#FF573"
+    expect(functions.isHexColor(invalidHex)).toBe(false);
+  });
+test('[isHexColor] tests invalid hex color, invalid hex character', () => {
+    // Test invalid hex, invalid hex character
+    const invalidHex = "$FF5733";
+    expect(functions.isHexColor(invalidHex)).toBe(false);
+  });
+test('[isHexColor] tests valid hex color', () => {
+    // Test valid hex
+    const validHex = "#FF5733";
+    expect(functions.isHexColor(validHex)).toBe(true);
+  });
+test('[isHexColor] tests invalid hex color, missing leading hashtag', () => {
+    // Test valid hex
+    const validHex = "#FF3744";
+    expect(functions.isHexColor(validHex)).toBe(true);
+  });
